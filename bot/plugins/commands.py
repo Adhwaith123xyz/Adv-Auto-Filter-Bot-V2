d482a5381d9d92c40bf7f3de55a03af69609b4a9
Adhwaith123xyz <88830027+Adhwaith123xyz@users.noreply.github.com>
2021-08-15 13:21:52 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔥MY GROOP🔥', url="https://t.me/ADMOVEIAD"
                                 )
                         ]
                     ]
@@ -50,7 +50,7 @@ async def start(bot, update):
         InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/@ADMOVEIAD')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
